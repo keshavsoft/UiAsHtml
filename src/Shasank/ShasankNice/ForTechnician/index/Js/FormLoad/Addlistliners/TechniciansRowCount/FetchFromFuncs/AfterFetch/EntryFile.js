495578c559d13f4ce9622ef315fd3bc8bd392324
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2024-02-10 11:48:25 +0530

    SREENIVAS

--- src/Shasank/ShasankNice/ForTechnician/index/Js/FormLoad/Addlistliners/TechniciansRowCount/FetchFromFuncs/AfterFetch/EntryFile.js
+++ src/Shasank/ShasankNice/ForTechnician/index/Js/FormLoad/Addlistliners/TechniciansRowCount/FetchFromFuncs/AfterFetch/EntryFile.js
@@ -7,9 +7,13 @@ let LocalFuncForUsersRowCountId = ({ inData }) => {
     let jVarLocalEngineer = getUrlQueryParams({ inGetKey: "Engineer" });
     let jVarLocalTechniciansData = inData.filter(element => element.Engineer === jVarLocalEngineer && element.InternalStatus == "Allocated")
     let jVarLocalUsersRowCountId = document.getElementById('TechniciansRowCountId');
+    let jVarLocalNotificationId = document.getElementById('NotificationId');
 
     if (jVarLocalUsersRowCountId === null === false) {
         jVarLocalUsersRowCountId.innerHTML = jVarLocalTechniciansData.length
+    };
+    if (jVarLocalNotificationId === null === false) {
+        jVarLocalNotificationId.innerHTML = jVarLocalTechniciansData.length
     };
 };
 
