753d2671a19d4a38acab14db139a3881193b5820
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2024-02-13 11:25:14 +0530

    SREENIVAS

--- src/LaundryPos/NewOrder/ToStaticDom/AddListenerFuncs/StartFunc.js
+++ src/LaundryPos/NewOrder/ToStaticDom/AddListenerFuncs/StartFunc.js
@@ -1,9 +1,10 @@
 import { StartFunc as StartFuncCategoryRadio } from "./CategoryRadio/StartFunc.js";
 import { StartFunc as StartFuncItemSelect } from "./ItemSelect/OnChange/StartFunc.js";
-import { StartFunc as StartFuncForLastOrder } from "../FetchFuncs/ForLastOrder/EntryFile.js";
+// import { StartFunc as StartFuncForLastOrder } from "../FetchFuncs/ForLastOrder/EntryFile.js";
 
 let StartFunc = () => {
-    StartFuncForLastOrder().then();
+    // sreenivas 13/12/2024
+    // StartFuncForLastOrder().then();
     StartFuncItemSelect();
     StartFuncCategoryRadio();
 };
